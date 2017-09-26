4ea2c3e9834d4a550b7cc24f97eea6cd2eeb4b92
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-09-26 21:45:14 +0300

    Remove ads and in-app purchases related keys from private data template

--- app/src/main/java/fi/tuska/jalkametri/PrivateDataTemplate.java
+++ app/src/main/java/fi/tuska/jalkametri/PrivateDataTemplate.java
@@ -23,26 +23,4 @@ public interface PrivateDataTemplate {
      */
     boolean LOGGING_ENABLED = false;
 
-    /** AdMob publisher ID */
-    String ADMOB_PUBLISHER_ID = "my-admob-publisher-id";
-
-    /** The real product ID */
-    String JALCOMETER_REAL_LICENSE_PRODUCT_ID = "product_id_in_android_market";
-    /** Test product IDs */
-    String TEST_PRODUCT_ID_PURCHASED = "android.test.purchased";
-    String TEST_PRODUCT_ID_CANCELED = "android.test.canceled";
-    String TEST_PRODUCT_ID_REFUNDED = "android.test.refunded";
-    String TEST_PRODUCT_ID_ITEM_UNAVAILABLE = "android.test.item_unavailable";
-    /** Product ID for the jAlcoMeter license */
-    String LICENSE_PRODUCT_ID = JALCOMETER_REAL_LICENSE_PRODUCT_ID;
-
-    /** License purchasing payload */
-    String LICENSE_PURCHASE_PAYLOAD = "license_purchasing_payload";
-
-    /** Form key of the Google Docs form where stack traces are sent */
-    String STACK_TRACE_DOCS_FORM_KEY = "form_key_for_google_docs_document";
-
-    /** The Market account public key for verification */
-    String ACCOUNT_PUBLIC_KEY = "android_market_public_key";
-
 }
