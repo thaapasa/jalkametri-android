88aff4c5e54467d0e36965c7d0bb2d66b40c3ffc
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-10-15 13:26:50 +0300

    Copy locale helper code from teh internets

---
+++ app/src/main/java/fi/tuska/jalkametri/util/LocaleHelper.kt
@@ -0,0 +1,81 @@
+package fi.tuska.jalkametri.util
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.preference.PreferenceManager
+import java.util.Locale
+
+/**
+ * Locale helper code copied from
+ * https://gunhansancar.com/change-language-programmatically-in-android/
+ */
+class LocaleHelper {
+
+    private val SELECTED_LANGUAGE_KEY = "Locale.Helper.Selected.Language"
+
+    fun onAttach(context: Context): Context {
+        val lang = getPersistedData(context, Locale.getDefault().getLanguage())
+        return setLocale(context, lang)
+    }
+
+    fun onAttach(context: Context, defaultLanguage: String): Context {
+        val lang = getPersistedData(context, defaultLanguage)
+        return setLocale(context, lang)
+    }
+
+    fun getLanguage(context: Context): String {
+        return getPersistedData(context, Locale.getDefault().getLanguage())
+    }
+
+    fun setLocale(context: Context, language: String): Context {
+        persist(context, language)
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            updateResources(context, language)
+        } else updateResourcesLegacy(context, language)
+
+    }
+
+    private fun getPersistedData(context: Context, defaultLanguage: String): String {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        return preferences.getString(SELECTED_LANGUAGE_KEY, defaultLanguage)
+    }
+
+    private fun persist(context: Context, language: String) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = preferences.edit()
+
+        editor.putString(SELECTED_LANGUAGE_KEY, language)
+        editor.apply()
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun updateResources(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val configuration = context.getResources().getConfiguration()
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+
+        return context.createConfigurationContext(configuration)
+    }
+
+    private fun updateResourcesLegacy(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val resources = context.getResources()
+
+        val configuration = resources.getConfiguration()
+        configuration.locale = locale
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale)
+        }
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics())
+
+        return context
+    }
+}
